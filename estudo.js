d754b1fea4cd8c75f3b5cecd7485edf5249844ff
Peter.html <106194073+PedroCamisa10@users.noreply.github.com>
2022-10-04 17:44:52 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -17,3 +17,20 @@ function alo(){
     document.write(tabuada + " x 10 = " + (tabuada*10));
        
 }
+function escreva(){
+  for(var i=1; i <= 10; i++){
+     for(var j = 1; j <=10; j++){
+document.write(i + " x " + j + " = " + (j*i)+"<br>");
+     }
+    document.write("<br>");
+ }
+
+}
+
+function quadrado(){
+    for(var i = 2; i < 11; i++){
+        document.write("O quadrado de " + i + " é " + (i*i)+"<br><!DOCTYPE html>
+  
+        
+    }
+}
